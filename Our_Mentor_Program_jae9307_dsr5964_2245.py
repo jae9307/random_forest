f3c850013b34b09681a94c3c93691bd7e9e3e220
Daniel S Rigoglioso <dsr5964@g.rit.edu>
2025-04-24 19:39:05 -0400

    Finished classifier program but accuracy is low so we are trying to implement N fold cross validation

--- Our_Mentor_Program_jae9307_dsr5964_2245.py
+++ Our_Mentor_Program_jae9307_dsr5964_2245.py
@@ -40,22 +40,68 @@ def find_threshold_index(index, drivers, threshold, current_best_index, attribut
 def make_decision_stub(records, decision_stubs):
     # TODO: change this to 7 once you figure out how to handle the binary attribute
     random_attribute_number = random.randrange(6)
-    sorted_records = sorted(records, key=lambda single_record: single_record[random_attribute_number])
+
+    bhutan_records = []
+    assam_records = []
+    for record in records:
+        if record[7] == "Bhuttan":
+            bhutan_records.append(record)
+        if record[7] == "Assam":
+            assam_records.append(record)
+
+    # maybe grab the 2/3 records in a random order?
+    training_data_bhutan_len = math.floor(len(bhutan_records) * (2/3))
+    training_data_assam_len = math.floor(len(assam_records) * (2/3))
+
+    training_bhutan_records = [bhutan_records[i] for i in range(training_data_bhutan_len)]
+    training_assam_records = [assam_records[i] for i in range(training_data_assam_len)]
+
+    validation_bhutan_records = [bhutan_records[i] for i in range(training_data_bhutan_len, len(bhutan_records))]
+    validation_assam_records = [assam_records[i] for i in range(training_data_assam_len, len(assam_records))]
+
+    training_records = training_assam_records + training_bhutan_records
+    validation_records = validation_assam_records + validation_bhutan_records
+    print("Val record length ", len(validation_records))
+
+    sorted_records = sorted(training_records, key=lambda single_record: single_record[random_attribute_number])
     random_threshold = random.uniform(sorted_records[0][random_attribute_number],
                                       sorted_records[-1][random_attribute_number])
-    # TODO: change how you handle threshold for binary attribute (earlobes)
+
     threshold_index = find_threshold_index(math.ceil(records.__len__() / 2) - 1, sorted_records,
                                            random_threshold, -1, random_attribute_number)
 
     # create the groups left and right of the threshold using the threshold_index
     left_data = sorted_records[:threshold_index + 1]
-    # if random_attribute_number == 6:  # the attribute at index 6 is EarLobes, a binary value attribute
-    #     left_data = list(filter(lambda record: record[random_attribute_number] == 0, records))
+    if random_attribute_number == 6:  # the attribute at index 6 is EarLobes, a binary value attribute
+        # if the attribute is binary set the random threshold to 0
+        left_data = list(filter(lambda record: record[random_attribute_number] == 0, records))
+        random_threshold = 0
 
     left_data_bhuttan_count = sum(int(record[-1]) == 1 for record in left_data)
     left_data_assam_count = sum(int(record[-1]) == -1 for record in left_data)
     left_is_assam = left_data_assam_count > left_data_bhuttan_count
 
+    mistakes = 0
+    for record in validation_records:
+        is_assam = False
+        if left_is_assam:
+            if record[random_attribute_number] <= random_threshold:
+                is_assam = True
+            else:
+                is_assam = False
+        else:
+            if record[random_attribute_number] <= random_threshold:
+                is_assam = False
+            else:
+                is_assam = True
+        if is_assam and record[7] == "Bhuttan":
+            mistakes += 1
+        if not is_assam and record[7] == "Assam":
+            mistakes += 1
+
+    print("Mistakes ", mistakes)
+    accuracy = len(validation_records) - mistakes / len(validation_records)
+    print("Accuracy ", accuracy)
     # TODO: make sure classifier is at least 50% accurate
 
     decision_stubs.append(Decision_Stub(left_is_assam, random_threshold, random_attribute_number))
@@ -67,9 +113,10 @@ import os
 import re
 import numpy as np
 import argparse
+import pandas as pd
 
-def read_file():
-    file_name = "Abominable_Data_HW_LABELED_TRAINING_DATA__v772_2245.csv"
+def read_file(file_name):
+    #file_name = "Abominable_Data_HW_LABELED_TRAINING_DATA__v772_2245.csv"
 
     csv_data = pd.read_csv(file_name)
 
@@ -82,12 +129,9 @@ def classifier():
 
     records = read_file(args.filename)
 
-    classification_file = open("Abominable_Data_HW_LABELED_TRAINING_DATA__v772_2245.csv", "w")
-    classification_file.write("")
-    classification_file.close()
-    
-    assam_votes = 0
-    bhuttan_votes = 0
+    #classification_file = open("Classifications.csv", "w")
+    #classification_file.write("")
+    #classification_file.close()
     
     """
 
@@ -109,15 +153,17 @@ if __name__ == '__main__':
 def write_decision_conditionals(decision_stubs):
     decisions_string = ""
 
-    # TODO: fix formatting
+    for_string = (f"    for record in records:\n"
+                  f"        assam_votes = 0\n"
+                  f"        bhuttan_votes = 0\n")
     for stub in decision_stubs:
-        if_string = f"if record[{stub.attribute}] <= {stub.threshold}:\n"
-        if_vote_string = "    assam_votes += 1" if stub.left_is_assam_flag else "bhuttan_votes += 1\n"
-        else_string = f"else:\n"
-        else_vote_string = "    bhuttan_votes += 1" if stub.left_is_assam_flag else "assam_votes += 1\n"
+        if_string = f"        if record[{stub.attribute}] <= {stub.threshold}:\n"
+        if_vote_string = "            assam_votes += 1\n" if stub.left_is_assam_flag else "            bhuttan_votes += 1\n"
+        else_string = f"        else:\n"
+        else_vote_string = "            bhuttan_votes += 1\n" if stub.left_is_assam_flag else "            assam_votes += 1\n"
         decisions_string += if_string + if_vote_string + else_string + else_vote_string
-
-    return decisions_string
+    print_majority_class_string = f"        print('Assam' if assam_votes > bhuttan_votes else 'Bhutan')\n"
+    return for_string + decisions_string + print_majority_class_string
 
 def main():
     records = read_file()
